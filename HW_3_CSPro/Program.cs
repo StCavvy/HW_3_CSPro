d637bedc9325b3adf6bb95a2b0bbe7d97e59ff4b
Pavlo Eksarov <saintcavvy@gmail.com>
2023-09-19 20:58:24 +0300

    user1 created; added "ShowUser" method

--- HW_3_CSPro/Program.cs
+++ HW_3_CSPro/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace HW_3_CSPro
+﻿using System.Text;
+
+namespace HW_3_CSPro
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            //додаємо українську мову для коректного відображення в консолі
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //створюємо об'єкт класу User
+            User user1 = new User(20);
+
+            //задаємо стать
+            user1.gender_is_male = true;
+
+            //реалізація вводу імені та прізвища
+            Console.Write("Ім'я : ");
+            user1.Firstname = Console.ReadLine();
+
+            Console.Write("Прізвище : ");
+            user1.Lastname = Console.ReadLine();
+
+            //чистимо консоль, аби не заважало
+            Console.Clear();
+
+            //визиваємо метод, який відобразить данні нашого юзера
+            user1.ShowUser();
         }
     }
 }
--- HW_3_CSPro/User.cs
+++ HW_3_CSPro/User.cs
@@ -8,11 +8,14 @@ namespace HW_3_CSPro
 {
     internal class User
     {
+        //проперті
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public byte Age { get; }
         public bool gender_is_male { get; set; }
 
+
+        //дефолтний конструктор
         public User()
         {
             Firstname = "Невідомо";
@@ -21,12 +24,26 @@ namespace HW_3_CSPro
             gender_is_male = true;
         }
 
+
+        //конструктор з можливістю вказати пропертю Age
         public User (byte Age)
         {
             Firstname = "Невідомо";
             Lastname = "Невідомо";
             this.Age = Age;
             gender_is_male = true;
+        }
+        
+
+        //метод, який відображає дані про юзера
+        public void ShowUser()
+        {
+            Console.WriteLine("Ім'я : {0}", Firstname);
+            Console.WriteLine("Прізвище : {0}", Lastname);
+            Console.WriteLine("Рік : {0}", Age);
+            Console.Write("Стать : ");
+            if (gender_is_male == true) { Console.WriteLine("чоловіча"); }
+            else { Console.WriteLine("жіноча"); }
         }
 
     }
